f3f4e168dd9d66f53b77fc213c6c1a032d9ce2ec
TheFlyingPizzaHuose <85021898+TheFlyingPizzaHuose@users.noreply.github.com>
2026-01-25 15:40:55 -0600

    Add files via upload

---
+++ Ground_Support/socket_server.js
@@ -0,0 +1,34 @@
+const http = require("http");
+const fs = require("fs");
+const path = require("path");
+const { Server } = require("socket.io");
+
+const server = http.createServer((req, res) => {
+  if (req.url === "/chart.js") {
+    const filePath = path.join(
+      __dirname,
+      "node_modules/chart.js/dist/chart.umd.min.js"
+    );
+
+    res.writeHead(200, {
+      "Content-Type": "application/javascript",
+      "X-Content-Type-Options": "nosniff",
+    });
+
+    fs.createReadStream(filePath).pipe(res);
+    return;
+  }
+
+  res.writeHead(404);
+  res.end();
+});
+
+const io = new Server(server, {
+  cors: { 
+    origin: "http://127.0.0.1:4000" ,
+    methods: ["GET", "POST"],
+    credentials: false,
+  },
+});
+
+server.listen(3000);
